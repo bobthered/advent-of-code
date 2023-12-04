a0c3a56e3bf0f82a794ba4f467351b9e8a9777b5
Bob McAleavey <bobmcaleavey@gmail.com>
2023-12-04 09:25:17 -0500

    feat(3-Gear Ratios): add Part Two

--- 3-gear-ratios/solution.js
+++ 3-gear-ratios/solution.js
@@ -43,3 +43,61 @@ export const partOne = (input = '') => {
 
   return partNumbersSum;
 }
+
+export const partTwo = (input = '') => {
+  // parse input
+  const lines = input.split('\n');
+
+  // initialize gear ratios
+  let gearRatios = [];
+
+  // loop through lines
+  lines.map((line, rowIndex) => {
+    // find all gears
+    const gears = [...line.matchAll(/\*/g)];
+
+    // loop over possible gears
+    gears.map(gear => {
+      // destructure gear data
+      const { index: gearIndex } = gear;
+
+      // look for adjacent numbers
+      const adjacentNumbers = [...Array(3)].reduce((adjacentNumbers, _, rowIndexDelta) => {
+        // get current rowIndex
+        const currentRowIndex = rowIndex + rowIndexDelta - 1;
+
+        // find all numbers around gear
+        const matches = [...lines[currentRowIndex]?.matchAll(/\d+/g)];
+
+        // loop over numbers
+        matches.map(match => {
+          // destructure match data
+          const [number] = match;
+          const { index: numberIndex } = match;
+
+          // find bounds
+          const isGreaterThan = gearIndex >= numberIndex - 1;
+          const isLessThan = gearIndex < numberIndex + number.length + 1
+
+          // check if is adjacent to gearIndex
+          if (isGreaterThan && isLessThan) {
+            adjacentNumbers = [...adjacentNumbers, number]
+          }
+        })
+
+
+        return adjacentNumbers
+      }, [])
+
+      // check if exactly two numbers are adjacent
+      if (adjacentNumbers.length === 2) gearRatios.push(adjacentNumbers.reduce((total, number) => total *= +number))
+
+    })
+
+  })
+
+  // get sum of gear ratios
+  const gearRatiosSum = gearRatios.reduce((total, current) => total += +current, 0)
+
+  return gearRatiosSum
+}
