552e120aa32f1c29e6706ae43341a6414b1f6107
Bob McAleavey <bobmcaleavey@gmail.com>
2024-12-03 14:35:04 -0500

    feat: 3-Mull It Over - refactor part two

--- 2024/3-mull-it-over/solution.js
+++ 2024/3-mull-it-over/solution.js
@@ -9,31 +9,15 @@ export const partOne = (input = "") => {
 };
 
 export const partTwo = (input = "") => {
-  let characterIndex = 0;
-  let instructionsEnabled = true;
-  let total = 0;
-
-  while (characterIndex < input.length - 0) {
-    let inputSubstring = input.substring(characterIndex + 1);
-    const {
-      0: match,
-      1: x,
-      2: y,
-      index: matchIndex,
-    } = inputSubstring.match(/mul\((\d{1,3}),(\d{1,3})\)|don\'t\(\)|do\(\)/) ||
-    {};
-    if (match === undefined) break;
-    if (x !== undefined && y !== undefined && instructionsEnabled) {
-      total += +x * +y;
-    }
-    if (match === "don't()") {
-      instructionsEnabled = false;
-    }
-    if (match === "do()") {
-      instructionsEnabled = true;
-    }
-    characterIndex += match.length + matchIndex;
-  }
-
+  const regex = /mul\((\d{1,3}),(\d{1,3})\)|don\'t\(\)|do\(\)/g;
+  const matches = [...input.matchAll(regex)];
+  let enabled = true;
+  const total = matches.reduce((total, match) => {
+    const { 0: string, 1: x, 2: y } = match;
+    if (string === "don't()") enabled = false;
+    if (string === "do()") enabled = true;
+    if (x !== undefined && enabled) total += +x * +y;
+    return total;
+  }, 0);
   return total;
 };
