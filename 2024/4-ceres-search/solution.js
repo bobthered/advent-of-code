5cb1a5084712c5f40252fd2c87937ba158b8eed2
Bob McAleavey <bobmcaleavey@gmail.com>
2024-12-04 07:38:02 -0500

    feat: 4-Ceres Search - add part two

--- 2024/4-ceres-search/solution.js
+++ 2024/4-ceres-search/solution.js
@@ -1,17 +1,18 @@
-const directions = [
-  { x: 1, y: 0 }, // right
-  { x: 0, y: 1 }, // down
-  { x: -1, y: 0 }, // left
-  { x: 0, y: -1 }, // up
-  { x: 1, y: -1 }, // up & right
-  { x: 1, y: 1 }, // down & right
-  { x: -1, y: 1 }, // down & left
-  { x: -1, y: -1 }, // up & left
-];
-
 export const partOne = (input = "", searchWord = "XMAS") => {
+  const directions = [
+    { x: 1, y: 0 }, // right
+    { x: 0, y: 1 }, // down
+    { x: -1, y: 0 }, // left
+    { x: 0, y: -1 }, // up
+    { x: 1, y: -1 }, // up & right
+    { x: 1, y: 1 }, // down & right
+    { x: -1, y: 1 }, // down & left
+    { x: -1, y: -1 }, // up & left
+  ];
+
   const letters = input.split("\r\n").map((row) => row.split(""));
   let numberOfXMASoccurences = 0;
+
   for (let y = 0; y < letters.length; y++) {
     for (let x = 0; x < letters[y].length; x++) {
       if (letters[y][x] !== searchWord[0]) continue;
@@ -39,4 +40,56 @@ export const partOne = (input = "", searchWord = "XMAS") => {
   return numberOfXMASoccurences;
 };
 
-export const partTwo = (input = "") => {};
+export const partTwo = (input = "") => {
+  const directions = [
+    [
+      { character: "S", x: 1, y: -1 },
+      { character: "S", x: 1, y: 1 },
+      { character: "M", x: -1, y: -1 },
+      { character: "M", x: -1, y: 1 },
+    ],
+    [
+      { character: "S", x: -1, y: -1 },
+      { character: "S", x: -1, y: 1 },
+      { character: "M", x: 1, y: -1 },
+      { character: "M", x: 1, y: 1 },
+    ],
+    [
+      { character: "S", x: -1, y: -1 },
+      { character: "S", x: 1, y: -1 },
+      { character: "M", x: -1, y: 1 },
+      { character: "M", x: 1, y: 1 },
+    ],
+    [
+      { character: "S", x: -1, y: 1 },
+      { character: "S", x: 1, y: 1 },
+      { character: "M", x: -1, y: -1 },
+      { character: "M", x: 1, y: -1 },
+    ],
+  ];
+  const letters = input.split("\r\n").map((row) => row.split(""));
+  let numberOfXMASoccurences = 0;
+
+  for (let y = 0; y < letters.length; y++) {
+    for (let x = 0; x < letters[y].length; x++) {
+      if (letters[y][x] !== "A") continue;
+      for (let direction of directions) {
+        let searchWordFound = true;
+        for (let characterLookup of direction) {
+          const character = characterLookup.character;
+          const searchY = y + characterLookup.y;
+          const searchX = x + characterLookup.x;
+          if (letters?.[searchY]?.[searchX] !== character) {
+            searchWordFound = false;
+            break;
+          }
+        }
+        if (searchWordFound) {
+          numberOfXMASoccurences++;
+        }
+      }
+    }
+  }
+
+  return numberOfXMASoccurences;
+};
