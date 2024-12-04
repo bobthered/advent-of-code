7b010d09eba706f2987a2c353f9de0f78e296657
Bob McAleavey <bobmcaleavey@gmail.com>
2024-12-04 13:06:31 -0500

    feat: 5-Doesn't He Have Intern-Elves For This? - add part two

--- 2015/5-doesnt-he-have-intern-elves-for-this/solution.js
+++ 2015/5-doesnt-he-have-intern-elves-for-this/solution.js
@@ -1,20 +1,35 @@
+const hasForbiddenCharacters = (input = "") => /ab|cd|pq|xy/g.test(input);
+const hasOneRepeatingCharacterWithExactlyOneLetterBetween = (input = "") =>
+  /([a-z]).\1/g.test(input);
+const hasPairOfLetters = (input = "") => /([a-z]{2}).{0,}\1/g.test(input);
 const hasRepeatingLetters = (input = "") => /(.)\1{1,}/g.test(input);
 const hasThreeVowels = (input = "") =>
   /[a|e|i|o|u].{0,}[a|e|i|o|u].{0,}[a|e|i|o|u]/g.test(input);
-const hasForbiddenCharacters = (input = "") => /ab|cd|pq|xy/g.test(input);
-
-const isANiceString = (input = "") => {
-  if (!hasThreeVowels(input)) return false;
-  if (!hasRepeatingLetters(input)) return false;
-  if (hasForbiddenCharacters(input)) return false;
-  return true;
-};
 
 export const partOne = (input = "") => {
+  const isANiceString = (input = "") => {
+    if (!hasThreeVowels(input)) return false;
+    if (!hasRepeatingLetters(input)) return false;
+    if (hasForbiddenCharacters(input)) return false;
+    return true;
+  };
+
   const niceStrings = input
     .split("\r\n")
     .filter((string) => isANiceString(string));
   return niceStrings.length;
 };
 
-export const partTwo = (input = "") => {};
+export const partTwo = (input = "") => {
+  const isANiceString = (input = "") => {
+    if (!hasPairOfLetters(input)) return false;
+    if (!hasOneRepeatingCharacterWithExactlyOneLetterBetween(input))
+      return false;
+    return true;
+  };
+
+  const niceStrings = input
+    .split("\r\n")
+    .filter((string) => isANiceString(string));
+  return niceStrings.length;
+};
