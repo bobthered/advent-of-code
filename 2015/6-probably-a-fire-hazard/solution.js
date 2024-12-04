629d8fae9e3129ff7d509f54cacdff4d11d60d46
Bob McAleavey <bobmcaleavey@gmail.com>
2024-12-04 13:28:51 -0500

    feat: 6-Probably a Fire Hazard - add part two

--- 2015/6-probably-a-fire-hazard/solution.js
+++ 2015/6-probably-a-fire-hazard/solution.js
@@ -25,4 +25,29 @@ export const partOne = (input = "") => {
   return totalLightsLit;
 };
 
-export const partTwo = (input = "") => {};
+export const partTwo = (input = "") => {
+  const grid = [...Array(1000)].map((_) => [...Array(1000)].map((_) => 0));
+  const instructions = input.split("\r\n");
+  instructions.forEach((instruction) => {
+    const {
+      1: operation,
+      2: x1,
+      3: y1,
+      4: x2,
+      5: y2,
+    } = instruction.match(
+      /(turn\son|turn\soff|toggle)\s(\d+),(\d+)\sthrough\s(\d+),(\d+)/
+    );
+    for (let y = +y1; y <= +y2; y++) {
+      for (let x = +x1; x <= +x2; x++) {
+        if (operation === "turn off") grid[y][x] = Math.max(0, grid[y][x] - 1);
+        if (operation === "turn on") grid[y][x]++;
+        if (operation === "toggle") grid[y][x] = grid[y][x] + 2;
+      }
+    }
+  });
+  const totalLightsLit = grid
+    .flat(1)
+    .reduce((total, lightValue) => total + lightValue, 0);
+  return totalLightsLit;
+};
