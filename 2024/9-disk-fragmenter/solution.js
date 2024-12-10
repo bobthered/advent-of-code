ba6b6cb3d9339d6eb4f352adc0d18653562887c4
Bob McAleavey <bobmcaleavey@gmail.com>
2024-12-10 08:35:48 -0500

    feat: 9-Disk Fragmenter - add part two

--- 2024/9-disk-fragmenter/solution.js
+++ 2024/9-disk-fragmenter/solution.js
@@ -1,11 +1,11 @@
 const convertDiskMapToBlocks = (diskMap) =>
-  diskMap.split("").reduce((total, string, index) => {
+  diskMap.split("").reduce((blocks, string, index) => {
     const numberOfRepeats = +string;
     const value = index % 2 === 0 ? Math.floor(index / 2) : "";
     for (let i = 0; i < numberOfRepeats; i++) {
-      total.push(value);
+      blocks.push(value);
     }
-    return total;
+    return blocks;
   }, []);
 
 const defragmentBlocks = (blocks) => {
@@ -42,4 +42,59 @@ export const partOne = (diskMap = "") => {
   return checkSum;
 };
 
-export const partTwo = (input = "") => {};
+export const partTwo = (diskMap = "") => {
+  const arr = [];
+
+  for (let i = 0; i < diskMap.length; i++) {
+    const fileSize = parseInt(diskMap[i * 2]);
+    const freeSpace = parseInt(diskMap[i * 2 + 1]);
+    for (let j = 0; j < fileSize; j++) arr.push(`${i}`);
+    for (let j = 0; j < freeSpace; j++) arr.push(".");
+  }
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (arr[i] !== ".") {
+      const blockIdxs = [];
+      for (let j = i; j >= 0; j--) {
+        if (arr[j] == arr[i]) {
+          blockIdxs.push(j);
+        } else {
+          for (let k = 0; k < i; k++) {
+            if (arr[k] === ".") {
+              let dotBlockIdxs = [];
+              for (let l = k; l < arr.length; l++) {
+                if (arr[l] === arr[k]) {
+                  dotBlockIdxs.push(l);
+                } else {
+                  if (blockIdxs.length <= dotBlockIdxs.length) {
+                    for (let m = 0; m < blockIdxs.length; m++) {
+                      if (blockIdxs[m] > dotBlockIdxs[m]) {
+                        const ch = arr[blockIdxs[m]];
+                        arr[blockIdxs[m]] = arr[dotBlockIdxs[m]];
+                        arr[dotBlockIdxs[m]] = ch;
+                      }
+                    }
+                    break;
+                  } else {
+                    dotBlockIdxs = [];
+                  }
+                }
+              }
+            }
+          }
+
+          i = j + 1;
+          break;
+        }
+      }
+    }
+  }
+
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== ".") {
+      sum += i * parseInt(arr[i]);
+    }
+  }
+  return sum;
+};
